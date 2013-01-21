f1e05781c13665119603dd4f3a02d571ed152c85
stefan.dobrev@gmail.com <stefan.dobrev@gmail.com>
2013-01-21 18:01:52 +0200

    Modifying the default app template to the samples one. Adding basic barcode scanner scan usage.

---
+++ sample-barcode-scanner/scripts/main.js
@@ -0,0 +1,45 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+	var app = new App();
+	app.run();
+}
+
+function App() {
+}
+
+App.prototype = {
+	resultsField: null,
+     
+	run: function() {
+		var that = this,
+		scanButton = document.getElementById("scanButton");
+        
+		that.resultsField = document.getElementById("result");
+        
+		scanButton.addEventListener("click",
+									function() { 
+										that._scan.call(that); 
+									});
+	},
+    
+	_scan: function() {
+		var that = this;
+		window.plugins.barcodeScanner.scan(
+			function(result) {
+				if (!result.cancelled) {
+					that._addMessageToLog(result.format + " | " + result.text);    
+				}
+			}, 
+			function(error) {
+				console.log("Scanning failed: " + error);
+			});
+	},
+
+	_addMessageToLog: function(message) {
+		var that = this,
+		currentMessage = that.resultsField.innerHTML;
+        
+		that.resultsField.innerHTML = currentMessage + message + '<br />'; 
+	}
+}
